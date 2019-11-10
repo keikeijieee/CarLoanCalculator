21885d587bbff44079c5ece6860cb0d7685af8fa
keikeijieee <keikeijieeee@gmail.com>
2019-11-10 22:37:14 +0800

    done

--- app/src/main/java/com/example/carloancalculator/MainActivity.kt
+++ app/src/main/java/com/example/carloancalculator/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,26 +27,30 @@ class MainActivity : AppCompatActivity() {
             editTextDownPayment.setError(getString(R.string.error_input))
             return
         }
-        if(editTextInterestRate.text.isEmpty()){
-            editTextInterestRate.setError(getString(R.string.error_input))
-            return
-        }
         if(editTextLoanPeriod.text.isEmpty()){
             editTextLoanPeriod.setError(getString(R.string.error_input))
             return
         }
-        
-        val carPrice: Int = editTextCarPrice.text.toString().toInt(); //convert to integer
-        val downPayment: Int = editTextDownPayment.text.toString().toInt();
-        val loan = carPrice - downPayment
-        val loanPeriod: Int =  editTextLoanPeriod.text.toString().toInt()
-        val interest: Float = editTextInterestRate.text.toString().toFloat() * carPrice * loanPeriod
-        val monthlyRepayment: Float = (carPrice + interest) / loanPeriod / 12
+        if(editTextInterestRate.text.isEmpty()){
+            editTextInterestRate.setError(getString(R.string.error_input))
+            return
+        }
 
+
+        val carPrice: Int = editTextCarPrice.text.toString().toInt() //convert to integer
+        val downPayment: Int = editTextDownPayment.text.toString().toInt()
+        val loan = carPrice - downPayment
 
         textViewLoan.setText(getString(R.string.loan) + "${loan}")
+
+        val loanPeriod: Int =  editTextLoanPeriod.text.toString().toInt()
+        val interest: Float = editTextInterestRate.text.toString().toFloat() * carPrice * loanPeriod
+
         textViewInterest.setText(getString(R.string.interest) + "${interest}")
-        textViewMonthlyRepayment.setText(getString(R.string.monthly_repayment) + "${monthlyRepayment}")
+
+        val monthlyRepayment: Float = (loan + interest) / loanPeriod / 12
+        val format = NumberFormat.getCurrencyInstance(resources.configuration.locale)
+        textViewMonthlyRepayment.setText(getString(R.string.monthly_repayment) + "${format.format(monthlyRepayment)}")
     }
 
     fun reset(view: View){
